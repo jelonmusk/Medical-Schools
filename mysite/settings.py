8a5c5a0d722c6a3d2c654e164e887d1a2a3efcc0
JShaikh <jelonmusk@gmail.com>
2020-08-05 07:59:32 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,3 +151,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
